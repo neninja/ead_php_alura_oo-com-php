582c1186d9c38fc533b217f466747d5bf1f4f0c1
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 13:35:43 -0300

    :100: add validação completa de cpf
    gerador de cpf: https://www.4devs.com.br/gerador_de_cpf

--- banco.php
+++ banco.php
@@ -4,7 +4,7 @@ require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$vinicius = new Titular(new CPF('797.828.800-51'), 'Vinicius Dias');
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +13,10 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('771.426.240-00'), 'Patricia');
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123'), 'Abcdefg'));
+$outra = new Conta(new Titular(new CPF('003.247.790-20'), 'Abcdefg'));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
--- src/CPF.php
+++ src/CPF.php
@@ -6,16 +6,39 @@ class CPF
 
     public function __construct(string $numero)
     {
-        $numero = filter_var($numero, FILTER_VALIDATE_REGEXP, [
-            'options' => [
-                'regexp' => '/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/'
-            ]
-        ]);
-        if ($numero === false) {
+        $this->validaCpf($numero);
+        $this->numero = $numero;
+    }
+
+    // THANKS: https://gist.github.com/rafael-neri/ab3e58803a08cb4def059fce4e3c0e40
+    public function validaCpf(string $cpf)
+    {
+        // Extrai somente os números
+        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+
+        // Verifica se foi informado todos os digitos corretamente
+        if (strlen($cpf) != 11) {
             echo "Cpf inválido";
             exit();
         }
-        $this->numero = $numero;
+
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            echo "Cpf inválido";
+            exit();
+        }
+
+        // Faz o calculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                echo "Cpf inválido";
+                exit();
+            }
+        }
     }
 
     public function recuperaNumero()
