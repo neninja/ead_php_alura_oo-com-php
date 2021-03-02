e9afbe1e89c467750edcc8375063485f662e3479
Felipe Silva <felipe.dacs@gmail.com>
2021-03-02 11:42:43 -0300

    :tv: add aula 4

--- src/Conta.php
+++ src/Conta.php
@@ -4,14 +4,36 @@ class Conta
 {
     public $cpfTitular;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
 
-    public function sacar(float $valorASacar)
+    public function sacar(float $valorASacar): void
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
-        } else {
-            $this->saldo -= $valorASacar;
+            return;
         }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        } 
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
     }
 }
